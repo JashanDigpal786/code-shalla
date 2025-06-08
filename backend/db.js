95e55b947e48f92008fe271878dbf585db08b7e7
Jashan <jashan8847683268@gmail.com>
2025-06-08 17:24:27 +0530

    Password updation

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb+srv://jashan:password@123@cluster0.y5pf8br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI = "mongodb+srv://jashan:password%40123@cluster0.y5pf8br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectToMongo = () => {
     mongoose.set('strictQuery', true)
